962bff7697fb94e1b554529e3b956e7ba49bc682
aaa859 <a15680409211@163.com>
2020-04-03 21:42:15 +0800

    first vue file

---
+++ app.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="app">
+    <date-picker :date="date"></date-picker>
+  </div>
+</template>
+<script>
+import DatePicker from './DatePicker'
+export default {
+  components: {
+    DatePicker
+  },
+  data() {
+    return {
+      date: new Date()
+    }
+  }
+}
+</script>
